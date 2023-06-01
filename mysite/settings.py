383c34df5829a06501f0874fb353b6161f884055
sarahelizabeth <semurray3@gmail.com>
2023-06-01 11:24:58 -0500

    added login and logout functionality

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+# LOGIN REDIRECT: redirect a successful login to blog homepage
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
